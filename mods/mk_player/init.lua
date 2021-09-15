5bfa4f30816e259205d5f114218bd8a4e603a5a5
unknown <24964441+wsor4035@users.noreply.github.com>
2021-09-15 13:39:20 -0400

    new player properties

--- mods/mk_player/init.lua
+++ mods/mk_player/init.lua
@@ -1,6 +1,11 @@
+minetest.settings:set_bool("enable_damage", false)
+
 minetest.register_on_newplayer(function(ObjectRef)
     ObjectRef:set_pos(vector.new(0,5,0))
     ObjectRef:override_day_night_ratio(1)
+    --while no menu
+    ObjectRef:get_inventory():add_item("main", "kartcar:kart")
+    minetest.chat_send_player(ObjectRef:get_player_name(), "use /gen_map to create a new map")
 end)
 
 minetest.register_on_joinplayer(function(ObjectRef, last_login)
