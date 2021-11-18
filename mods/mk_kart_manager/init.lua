9754f725150590cbce43fc7c3a5a0f118d97df9f
unknown <24964441+wsor4035@users.noreply.github.com>
2021-11-18 00:06:11 -0500

    proof of concept pane switcher

--- mods/mk_kart_manager/init.lua
+++ mods/mk_kart_manager/init.lua
@@ -1,3 +1,37 @@
+local function cosmetics(name, def)
+    local formspec = {
+        type = "container",
+        name = name,
+        x = def.pos[1],
+        y = def.pos[2],
+        {
+            type = "label",
+            label = "comsmetics content",
+            x = 1,
+            y = 1,
+        }
+    }
+
+    return formspec
+end
+
+local function configuration(name, def)
+    local formspec = {
+        type = "container",
+        name = name,
+        x = def.pos[1],
+        y = def.pos[2],
+        {
+            type = "label",
+            label = "configuration content",
+            x = 1,
+            y = 1,
+        }
+    }
+
+    return formspec
+end
+
 local function side_bar(name, def)
     local formspec = {
         type = "container",
@@ -7,13 +41,33 @@ local function side_bar(name, def)
         kuto.component.card("mk_cosmetics", {
             pos = {0.25,0.25},
             icon = "kart_inv.png",
-            ttl_name = "mkc_ttl_name",
+            ttl_name = "mkcm_ttl_name",
             ttl_text = "Kart Cosmetics",
-            ctt_name = "mkc_ctt_name",
+            ctt_name = "mkcm_ctt_name",
             ctt_text = "customize the look and feel of your kart here",
-            btn_name = "mkc_btn_name",
+            btn_name = "mkcm_btn_name",
             on_event = function(form, player, element)
-                minetest.chat_send_player(player:get_player_name(), minetest.colorize("yellow", "[kuto]: test output"))
+                minetest.chat_send_player(player:get_player_name(), minetest.colorize("yellow", "[mk]: cosmetic"))
+                local index = kuto.get_index_by_name(form, "mk_content")
+                form[index] = cosmetics("mk_content", {pos = {0,0}})
+
+                return form
+            end
+        }),
+        kuto.component.card("mk_configuration", {
+            pos = {0.25,2.25},
+            icon = "kart_inv.png",
+            ttl_name = "mkcf_ttl_name",
+            ttl_text = "Kart configuration",
+            ctt_name = "mkcf_ctt_name",
+            ctt_text = "customize config options",
+            btn_name = "mkcf_btn_name",
+            on_event = function(form, player, element)
+                minetest.chat_send_player(player:get_player_name(), minetest.colorize("yellow", "[mk]: config"))
+                local index = kuto.get_index_by_name(form, "mk_content")
+                form[index] = configuration("mk_content", {pos = {0,0}})
+
+                return form
             end
         }),
     }
@@ -52,13 +106,8 @@ local formspec = {
         middle_x = 4,
         middle_y = 4,
     },
-    side_bar("mk_sidebar", {pos = {10.5, 0}, scl_name = "test"}),
-    {
-        type = "label",
-        label = "test",
-        x = 1,
-        y = 1,
-    }
+    side_bar("mk_sidebar", {pos = {10.5, 0}}),
+    cosmetics("mk_content", {pos = {0,0}})
 }
 
 minetest.register_on_joinplayer(function(player, last_login)
